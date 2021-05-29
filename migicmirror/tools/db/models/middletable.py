bcaabff006ae03c0dd0081be6cf0faa328f98ace
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2021-05-29 22:13:14 +0800

    fix: 修复datetime默认值
    1. 使用sqlalchemy时需要使用函数调用方式
    2. 或可使用datetime.now

--- migicmirror/tools/db/models/middletable.py
+++ migicmirror/tools/db/models/middletable.py
@@ -14,7 +14,7 @@ mm_question_question = Table(
     "mm_question_question", Model.metadata,
     Column("similarity_with", BIGINT, ForeignKey("mm_question.id"), primary_key=True),
     Column("similarity_to", BIGINT, ForeignKey("mm_question.id"), primary_key=True),
-    Column("create_date", DateTime, default=func.now),
+    Column("create_date", DateTime, default=func.now()),
 )
 
 # 问题 <-> 所属类目
@@ -22,6 +22,6 @@ mm_question_tag = Table(
     "mm_question_tag", Model.metadata,
     Column("question_id", BIGINT, ForeignKey("mm_question.id"), primary_key=True),
     Column("tag_id", Integer, ForeignKey("mm_tag.id"), primary_key=True),
-    Column("create_date", DateTime, default=func.now),
+    Column("create_date", DateTime, default=func.now()),
 )
-  
+
