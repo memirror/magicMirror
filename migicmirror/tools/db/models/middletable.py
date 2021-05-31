f4677d47671a677ec878112c6ceda71b893d1fe3
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2021-05-31 11:13:19 +0800

    refactor: 优化多对多中间表结构
    1. 优化tag <-> question
    2. 增加 role <-> permission

--- migicmirror/tools/db/models/middletable.py
+++ migicmirror/tools/db/models/middletable.py
@@ -4,12 +4,13 @@
 
 from sqlalchemy import Table
 from sqlalchemy import Column, ForeignKey
-from sqlalchemy import BIGINT, DateTime, Integer
+from sqlalchemy import BIGINT, DateTime, Integer, String
 from sqlalchemy import func
 
 from . import Model
 
-# 相似问题中间表
+
+# 问题 <-> 相似问题
 mm_question_question = Table(
     "mm_question_question", Model.metadata,
     Column("similarity_with", BIGINT, ForeignKey("mm_question.id"), primary_key=True),
@@ -21,7 +22,16 @@ mm_question_question = Table(
 mm_question_tag = Table(
     "mm_question_tag", Model.metadata,
     Column("question_id", BIGINT, ForeignKey("mm_question.id"), primary_key=True),
-    Column("tag_id", Integer, ForeignKey("mm_tag.id"), primary_key=True),
+    Column("tag", String(64), ForeignKey("mm_tag.tag"), primary_key=True),
     Column("create_date", DateTime, default=func.now()),
+)
+
+# 角色 <-> 权限
+mm_role_permission = Table(
+    "mm_role_permission", Model.metadata,
+    Column("role", String(64), ForeignKey("mm_role.role"), primary_key=True),
+    Column("permission", String(128), ForeignKey("mm_permission.permission"), primary_key=True),
+    Column("create_date", DateTime, default=func.now())
+
 )
 
