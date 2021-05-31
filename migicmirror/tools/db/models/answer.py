a94720454fa0e5d1c3108fd58bfd53c67b600ff5
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2021-05-31 11:13:41 +0800

    refactor: 优化表结构

--- migicmirror/tools/db/models/answer.py
+++ migicmirror/tools/db/models/answer.py
@@ -2,30 +2,30 @@
 # @Author: xiaodong
 # @Date  : 2021/5/29
 
-import enum
+import uuid
 
 from sqlalchemy import Column
 from sqlalchemy import ForeignKey
-from sqlalchemy import String, Integer, DateTime, Boolean, Text, BIGINT
+from sqlalchemy import Integer, Text, BIGINT, String
 from sqlalchemy_utils import ChoiceType, UUIDType
 
+from .question import Question
 from ..choices import ActionStatus, CheckStatus
-from . import Model, ModelMixin, ModelDateMixin, ModelDeleteMixin
+from . import Model, ModelMixin, ModelDateMixin, ModelDeleteMixin, _session
+
 
 class Answer(Model, ModelMixin, ModelDateMixin, ModelDeleteMixin):
 
-    __tablename__ = "mm_answer"
-
     id = Column(Integer, primary_key=True)
+    uid = Column(UUIDType(binary=False), unique=True, index=True, default=uuid.uuid4)
     answer = Column(Text, nullable=False)
 
     status = Column(ChoiceType(CheckStatus, impl=Integer()))
     action = Column(ChoiceType(ActionStatus, impl=Integer()))
 
-    create_by = Column(UUIDType(binary=False), ForeignKey("mm_user.id", ondelete="CASCADE"))
-    modify_by = Column(UUIDType(binary=False), ForeignKey("mm_user.id", ondelete="CASCADE"))
+    create_by = Column(String(64), ForeignKey("mm_user.name", ondelete="CASCADE"))
+    modify_by = Column(String(64), ForeignKey("mm_user.name", ondelete="CASCADE"))
 
-    role_id = Column(Integer, ForeignKey("mm_role.id", ondelete="CASCADE"))
     question_id = Column(BIGINT, ForeignKey("mm_question.id", ondelete="CASCADE"))
 
     def __init__(self, answer: str, status: int = 2, action: int = 1):
@@ -35,3 +35,7 @@ class Answer(Model, ModelMixin, ModelDateMixin, ModelDeleteMixin):
 
     def __str__(self):
         return self.answer
+
+    @property
+    def question(self):
+        return _session.query(Question).get(self.question_id).question
