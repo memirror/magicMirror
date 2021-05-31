2850b5f392500f3e7e8b49581029e7c92da7db7c
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2021-05-31 11:06:09 +0800

    refactor: 重构question表结构

--- migicmirror/tools/db/models/question.py
+++ migicmirror/tools/db/models/question.py
@@ -8,7 +8,7 @@ import uuid
 from sqlalchemy import Column
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy import String, Integer, DateTime, Boolean, BIGINT
+from sqlalchemy import String, Integer, BIGINT
 from sqlalchemy_utils.types import UUIDType, ChoiceType
 
 from ..choices import ActionStatus, CheckStatus
@@ -18,22 +18,19 @@ from .middletable import mm_question_question, mm_question_tag
 
 class Question(Model, ModelMixin, ModelDateMixin, ModelDeleteMixin):
 
-    __tablename__ = "mm_question"
-
     id = Column(BIGINT, primary_key=True)
     uid = Column(UUIDType(binary=False), unique=True, index=True, default=uuid.uuid4)
     question = Column(String(512), nullable=False, index=True, unique=True)
 
-    status = Column(ChoiceType(CheckStatus, impl=Integer()))
+    status = Column(ChoiceType(CheckStatus, impl=Integer()), default=1)
 
-    create_by = Column(UUIDType(binary=False), ForeignKey("mm_user.id", ondelete="CASCADE"))
-    modify_by = Column(UUIDType(binary=False), ForeignKey("mm_user.id", ondelete="CASCADE"))
+    create_by = Column(String(64), ForeignKey("mm_user.name", ondelete="CASCADE"), default="admin")
+    modify_by = Column(String(64), ForeignKey("mm_user.name", ondelete="CASCADE"))
 
-    action = Column(ChoiceType(ActionStatus, impl=Integer()))
-    role_id = Column(Integer, ForeignKey("mm_role.id", ondelete="CASCADE"))
+    action = Column(ChoiceType(ActionStatus, impl=Integer()), default=1)
 
-    tags = relationship("Tag", secondary=mm_question_tag, backref="questions")
-    answers = relationship("Answer", cascade="all, delete-orphan")
+    tags = relationship("Tag", secondary=mm_question_tag, back_populates="questions")
+    answers = relationship("Answer", cascade="all, delete-orphan", backref="question")
     similarity_with = relationship("Question", secondary=mm_question_question,
                                 primaryjoin=(mm_question_question.c.similarity_to == id),
                                 secondaryjoin=(mm_question_question.c.similarity_with == id),
