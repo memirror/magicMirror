40dcaef3079f25793676c836a12c567d2c2ac7ae
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2021-05-31 11:02:15 +0800

    refactor: 优化建表，删除记录字段
    1. 支持自动提取表名
    2. 将delete_date 字段转移至 ModelDeleteMixin
    3. 增加 create 函数用于返回数据

--- migicmirror/tools/db/models/__init__.py
+++ migicmirror/tools/db/models/__init__.py
@@ -2,22 +2,35 @@
 # @Author: xiaodong
 # @Date  : 2021/5/27
 
+import re
 from datetime import datetime
 
+from sqlalchemy.orm import scoped_session
 from sqlalchemy import DateTime, Column, Boolean
-from sqlalchemy.orm import relationship, scoped_session
+from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.ext.declarative import declarative_base
 
 from ..session import session, engine
 
-_session = scoped_session(session)
 
+_session = scoped_session(session)
 Model = declarative_base(name="Model")
+
+
+def camel2snake(name: str) -> str:
+    name = re.sub("[A-Z]", lambda obj: "_" + obj.group()[0].lower(), name)
+    if not name.startswith("_"):
+        name = "_" + name
+    return name
 
 
 class ModelMixin(object):
 
     query = objects = _session.query_property()
+
+    @declared_attr
+    def __tablename__(cls):
+        return "mm" + camel2snake(cls.__name__)
 
     def delete(self):
         _session.delete(self)
@@ -27,16 +40,19 @@ class ModelMixin(object):
         _session.add(self)
         _session.commit()
 
+    def create(self):
+        self.save()
+        return self
+
     def to_dict(self):
         return {
             obj: getattr(self, obj)
-            for obj in self.__class__.__table__.columns.keys()
+            for obj in type(self).__table__.columns.keys()
         }
 
 
 class ModelDateMixin(object):
 
-    delete_date = Column(DateTime, nullable=True)
     create_date = Column(DateTime, default=datetime.now)
     update_date = Column(DateTime, nullable=True, onupdate=datetime.now)
 
@@ -44,6 +60,8 @@ class ModelDateMixin(object):
 class ModelDeleteMixin(object):
 
     is_delete = Column(Boolean, default=False)
+    delete_date = Column(DateTime, nullable=True)
 
 
 from . import answer, middletable, permission, question, record, role, user, tag
+  
