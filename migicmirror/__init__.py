0df7021e123d0aac6d95e033f53470b9300f732e
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2021-06-01 15:25:44 +0800

    feature: 支持es，增加问答路由，支持相似比较过滤

--- migicmirror/__init__.py
+++ migicmirror/__init__.py
@@ -7,8 +7,11 @@ from typing import List, Dict, Any, Callable, Union
 from bisect import insort
 
 from .realtimespider import BaiduApi, SogouApi
-from .tools.whooshe import qw
+from .tools.whooshe import wq
 from .tools.db import Answer
+from .tools.es import esq
+from .tools.filter import SimpleFilter
+from .tools import similary
 
 
 Api: Callable[[str], str] = None
@@ -18,17 +21,32 @@ ApiDict: Dict[str, Api] = {}
 logger = logging.getLogger("magicmirror.realtimespider")
 
 
+SimpleFilter.callbacks = [
+    similary.token_set_ratio, similary.token_sort_ratio,
+]
+
+
+def qwsearch(question: str) -> Union[str]:
+    for obj in wq.search(question):
+        if SimpleFilter(question, obj["question"]).will_drop():
+            continue
+        answer = Answer.query.filter_by(question_id=obj["id"]).first()
+        return answer.answer if answer else None
+
+
+def qessearch(question: str) -> Union[str]:
+    for obj in esq.search(question):
+        if SimpleFilter(question, obj["question"]).will_drop():
+            continue
+        answer = Answer.query.filter_by(question_id=obj["id"]).first()
+        return answer.answer if answer else None
+
+
 class p(tuple):
     weight = property(lambda self: self[0])
     api = property(lambda self: self[1])
     source = property(lambda self: self[2])
     others = property(lambda self: self[3:])
-
-
-def qwsearch(question: str) -> Union[str]:
-    for obj in qw.search(question):
-        answer = Answer.query.filter_by(question_id=obj["id"]).first()
-        return answer.answer if answer else None
 
 
 def register(api: callable, source: str, weight, **kwargs):
@@ -39,3 +57,4 @@ def register(api: callable, source: str, weight, **kwargs):
 register(lambda q: BaiduApi(q)(), "baidu", 900)
 register(lambda q: SogouApi(q)(), "sogou", 800)
 register(qwsearch, "whoosh", 1000)
+register(qessearch, "elasticsearch", 2000)
