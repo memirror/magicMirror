5f8f8edfe81b5990dd335ac75d2741b28e940132
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2021-06-01 14:25:34 +0800

    Create query.py

---
+++ sql/query.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# @Author: xiaodong
+# @Date  : 2021/6/1
+
+from sqlalchemy import text, func
+
+from magicmirror.tools.db import _session
+from magicmirror.tools.db import User, Role, Permission
+from magicmirror.tools.db import Question, Answer, Tag
+from magicmirror.tools.db import Record
+from magicmirror.tools.db.models.middletable import mm_question_tag, mm_role_permission, mm_question_question
+
+
+most_tag = (
+    _session.query(mm_question_tag)
+    .with_entities(mm_question_tag.c.tag, func.count(mm_question_tag.c.tag).label("num"))
+    .group_by(mm_question_tag.c.tag)
+    .order_by(func.count(mm_question_tag.c.tag).desc())
+    .first()
+)
+
+question_with_most_answer = (
+    _session.query(Question).join(Answer, Answer.question_id == Question.id)
+    .with_entities(Question.question, text("count(1) num"), func.group_concat(Answer.answer, "|"))
+    .group_by(Answer.question_id, Question.question)
+    .order_by(text("count(1) desc"))
+    .first()
+)
+  
