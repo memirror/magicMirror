fddf629545f23101a3e2175cd248f0122d5a23c4
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2021-06-01 11:47:50 +0800

    Update __init__.py

--- migicmirror/tools/es/__init__.py
+++ migicmirror/tools/es/__init__.py
@@ -1,3 +1,13 @@
 # -*- coding: utf-8 -*-
 # @Author: xiaodong
 # @Date  : 2021/5/27
+
+from elasticsearch import Elasticsearch
+
+from .question import ElasticSearchQuestion
+from ...setting import ELASTICSEARCH_HOST
+
+
+ElasticSearchQuestion.es = Elasticsearch(ELASTICSEARCH_HOST)
+esq = ElasticSearchQuestion("mm_question")
+ 
