60203b730d444c926b055977b08c0b6194332b2a
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2021-06-01 10:15:53 +0800

    feature: 增加whoosh支持
    1. 增加whoosh问答支持
    2. 支持按权重或名称选择答案来源

--- migicmirror/__init__.py
+++ migicmirror/__init__.py
@@ -1,3 +1,41 @@
 # -*- coding: utf-8 -*-
 # @Author: xiaodong
 # @Date  : 2021/5/27
+
+import logging
+from typing import List, Dict, Any, Callable, Union
+from bisect import insort
+
+from .realtimespider import BaiduApi, SogouApi
+from .tools.whooshe import qw
+from .tools.db import Answer
+
+
+Api: Callable[[str], str] = None
+Apis: List[tuple] = []
+ApiDict: Dict[str, Api] = {}
+
+logger = logging.getLogger("magicmirror.realtimespider")
+
+
+class p(tuple):
+    weight = property(lambda self: self[0])
+    api = property(lambda self: self[1])
+    source = property(lambda self: self[2])
+    others = property(lambda self: self[3:])
+
+
+def qwsearch(question: str) -> Union[str]:
+    for obj in qw.search(question):
+        answer = Answer.query.filter_by(question_id=obj["id"]).first()
+        return answer.answer if answer else None
+
+
+def register(api: callable, source: str, weight, **kwargs):
+    ApiDict[source] = api
+    insort(Apis, p((-weight, api, source, kwargs)))
+
+
+register(lambda q: BaiduApi(q)(), "baidu", 900)
+register(lambda q: SogouApi(q)(), "sogou", 800)
+register(qwsearch, "whoosh", 1000)
