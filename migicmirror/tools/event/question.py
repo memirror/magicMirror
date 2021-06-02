0f03bbb355b0f69bc878479fb361ba2c70014f93
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2021-06-02 10:23:46 +0800

    featuer: 同步question表到whoosh、elasticsearch

---
+++ migicmirror/tools/event/question.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# @Author: xiaodong
+# @Date  : 2021/6/2
+
+from ..whooshe import wq
+from ..es import esq
+from ..signal import signal_insert_question, signal_delete_question
+
+
+@signal_insert_question.connect
+def insert_to_whoosh(target):
+    wq.write_obj(keyword=target.question, id=target.id)
+
+
+@signal_insert_question.connect
+def insert_to_elasticsearch(target):
+    esq.insert(body={
+        k: getattr(target, k)
+        for k in target.__table__.columns.keys()
+    }, id=target.id)
+
+
+@signal_delete_question.connect
+def delete_from_whoosh(target):
+    wq.delete_obj(keyword=target.question)
+
+
+@signal_delete_question.connect
+def delete_from_elasticsearch(target):
+    esq.delete_by_question_id(target.id)
+  
